fe7d07ee3c22431583e0adede87faffe134aeac3
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-18 21:40:04 +0100

    Added realtions to model

--- crmT/accounts/models.py
+++ crmT/accounts/models.py
@@ -9,6 +9,13 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100, null=True)
+	def __str__(self):
+		return self.name
+
+
 class Product(models.Model):
 	CATEGORY = (
 		('Indoor', 'Indoor'),
@@ -19,6 +26,11 @@ class Product(models.Model):
 	category = models.CharField(max_length=100, null=True, choices=CATEGORY)
 	description = models.TextField(max_length=500, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.name
+
 
 class Order(models.Model):
 	STATUS = (
@@ -26,7 +38,7 @@ class Order(models.Model):
 		('Out for delivery', 'Out for delivery'),
 		('Delivered', 'Delivered')
 		)
-	#customer = 
-	#product = 
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True) 
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)  
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=100, null=True, choices=STATUS)
