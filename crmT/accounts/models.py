cec14176210cb757084ba4d80820739031925042
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-18 20:51:23 +0100

    Created model for Customer, Product and Order

--- crmT/accounts/models.py
+++ crmT/accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=20, null=True)
+	email = models.EmailField(max_length=100, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	CATEGORY = (
+		('Indoor', 'Indoor'),
+		('Out Door', 'Out Door')
+		)
+	name = models.CharField(max_length=100, null=True)
+	price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+	category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+	description = models.TextField(max_length=500, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+	STATUS = (
+		('Pending', 'Pending'),
+		('Out for delivery', 'Out for delivery'),
+		('Delivered', 'Delivered')
+		)
+	#customer = 
+	#product = 
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=100, null=True, choices=STATUS)
