4c648a6efccb90a37a7215df46b8841f4cd121d5
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-19 11:48:52 +0100

    Completetion data for templates html files

--- crmT/accounts/urls.py
+++ crmT/accounts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home),
-    path('home/', views.home),
-    path('customers/<str:pk>/',views.customers),
-    path('products/', views.products)
+	path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('customers/<str:pk>/',views.customers, name='customers'),
+    path('products/', views.products, name='products')
 
 ]
