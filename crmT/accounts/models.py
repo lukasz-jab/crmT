4b24970785040aaecf2cf96b1c83b7127c5d5ec8
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-19 08:16:41 +0100

    Connected data withaccounts  templates html files

--- crmT/accounts/models.py
+++ crmT/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
-	name = models.CharField(max_length=200, null=True)
+	name = models.CharField(max_length=50, null=True)
 	phone = models.CharField(max_length=20, null=True)
 	email = models.EmailField(max_length=100, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
--- crmT/accounts/views.py
+++ crmT/accounts/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+	customers = Customer.objects.all()
+	orders = Order.objects.all()
+	total_orders = orders.count()
+	orders_delivered = orders.filter(status='Delivered').count()
+	orders_pending = orders.filter(status='Pending').count()
+
+	context = {
+		'customers': customers,
+		'orders': orders,
+		'total_orders': total_orders,
+		'orders_delivered': orders_delivered,
+		'orders_pending': orders_pending
+	}
+	return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-	return render(request, 'accounts/products.html')
+	obj = Product.objects.all()
+	context = {
+		'products': obj
+	}
+	return render(request, 'accounts/products.html', context)
 
 def customers(request):
-	return render(request, 'accounts/customers.html')
+	context = {
+
+	}
+	return render(request, 'accounts/customers.html', context)
